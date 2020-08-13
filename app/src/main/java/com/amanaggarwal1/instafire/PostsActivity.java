c9f51989e3ec31916d5ea0acf0848e7e3eb09aa4
Aman Aggarwal <amanaggarwal014@gmail.com>
2020-08-13 15:10:59 +0530

    Now data can be accessed from cloud firestore for posts and users

--- app/src/main/java/com/amanaggarwal1/instafire/PostsActivity.java
+++ app/src/main/java/com/amanaggarwal1/instafire/PostsActivity.java
@@ -1,20 +1,52 @@
 package com.amanaggarwal1.instafire;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.solver.widgets.Snapshot;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import org.w3c.dom.Document;
+
 public class PostsActivity extends AppCompatActivity {
+
+    FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_posts);
+
+        db = FirebaseFirestore.getInstance();
+        db.collection("users")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d("LOGCAT", document.getId() + " => " + document.getData());
+                            }
+                        } else {
+                            Log.w("LOGCAT", "Error getting documents.", task.getException());
+                        }
+                    }
+                });
     }
 
     @Override
