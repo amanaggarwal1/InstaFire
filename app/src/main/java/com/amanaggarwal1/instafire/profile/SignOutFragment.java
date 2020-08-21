ceb64bdbd435b4e7e80238c8eedc37da4045cff0
Aman Aggarwal <amanaggarwal014@gmail.com>
2020-08-21 18:44:39 +0530

    Added code to SignOutFragment so that users can sign out of current session

--- app/src/main/java/com/amanaggarwal1/instafire/profile/SignOutFragment.java
+++ app/src/main/java/com/amanaggarwal1/instafire/profile/SignOutFragment.java
@@ -1,22 +1,74 @@
 package com.amanaggarwal1.instafire.profile;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.amanaggarwal1.instafire.LoginActivity;
 import com.amanaggarwal1.instafire.R;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.io.File;
 
 public class SignOutFragment extends Fragment {
+    private static final String TAG = "SignOutFragment";
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_sign_out, container, false);
+
+        Button signOutButton = view.findViewById(R.id.sign_out_button);
+        signOutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                deleteCache(getContext());
+                Toast.makeText(getActivity(), "Logged out successfully", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(getActivity(), LoginActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+                getActivity().finish();
+            }
+        });
+
         return view;
     }
+
+    public static void deleteCache(Context context) {
+        try {
+            File dir = context.getCacheDir();
+            deleteDir(dir);
+            Log.d(TAG, "deleteCache: attempted to delete cache");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public static boolean deleteDir(File dir) {
+        if (dir != null && dir.isDirectory()) {
+            String[] children = dir.list();
+            for (int i = 0; i < children.length; i++) {
+                boolean success = deleteDir(new File(dir, children[i]));
+                if (!success) {
+                    return false;
+                }
+            }
+            return dir.delete();
+        } else if(dir!= null && dir.isFile()) {
+            return dir.delete();
+        } else {
+            return false;
+        }
+    }
+
 }
